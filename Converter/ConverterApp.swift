e50c3b889cc4cd76f1560a7486ffa10f11080bce
Eton Hsu <etonhsu111@gmail.com>
2024-02-27 09:14:41 -0800

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Eton  Hsu on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
